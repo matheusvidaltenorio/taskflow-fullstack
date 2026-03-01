9d6c03d9d7a776ff537d1a59eff9dce5296b5a06
Matheus Vidal Tenorio <matheustenoriovidal@gmail.com>
2026-03-01 06:18:55 -0300

    Ajustando porta para deploy

--- taskflow-backend/src/server.js
+++ taskflow-backend/src/server.js
@@ -88,4 +88,6 @@ app.delete("/tasks/:id", authMiddleware, (req, res) => {
   );
 });
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
